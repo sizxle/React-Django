a1cbbe501269973d980eb6d1c17e9da4c236bd51
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-10-30 14:24:58 +0200

    modified books model

--- server/backend/api/models.py
+++ server/backend/api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    release_year = models.DateField()
+    release_year = models.IntegerField()
 
     def __str__(self):
         return self.title
