3e078e62438ab1317126e8e107ae1fefc3d58fbe
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-10-30 18:37:24 +0200

    update urls

--- server/backend/api/urls.py
+++ server/backend/api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('books/', views.get_books, name="get_books"),
-    path('books/create/', views.create_book, name="create_book")
+    path('books/create/', views.create_book, name="create_book"),
+    path('books/delete/<int:id>', views.book_details, name="delete_book"),
+    path('books/update/<int:id>', views.book_details, name="update_book"),
 ]
