b7455c5c09b8e9e89b95bf9efa5d591a7b34c00c
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-10-30 17:36:49 +0200

    add books

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -3,25 +3,47 @@ import { useEffect, useState } from 'react'
 
 function App() {
 
-  const[books,setBooks] = useState([]);
+  const [books, setBooks] = useState([]);
+  const [title, setTitle] = useState("")
+  const [release_year, setReleaseYear] = useState(0)
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log("before use effect")
     fetchBooks();
     console.log("after use effect ")
-  },[])
+  }, [])
 
-  const fetchBooks = async()=>{
+  const fetchBooks = async () => {
 
-    try{
-      const response =await fetch("http://127.0.0.1:8000/api/books/");
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/books/");
       const data = await response.json()
-      console.log(data)
+      setBooks(data)
 
-    }catch(err){
+    } catch (err) {
       console.log(err)
     }
 
+  }
+
+  const handleAdd = async (e) => {
+    e.preventDefault()
+    console.log(e.target.title.value)
+    console.log(e)
+
+    const response = await fetch("http://127.0.0.1:8000/api/books/create/", {
+      method: 'POST',
+      body: JSON.stringify({
+        title: title,
+        release_year: release_year
+      }),
+      headers: {
+        'content-type': 'application/json;charset=UTF-8'
+      }
+    });
+
+    const data= await response.json()
+    setBooks((prev) => [...prev,data])
   }
 
   return (
@@ -29,11 +51,19 @@ function App() {
       <h1> Book Shop </h1>
 
       <div>
-        <input type='text' placeholder='Book Title' />
-        <input type='number' placeholder='Release date' />
-        <button> Add Book </button>
+        <form onSubmit={handleAdd} >
+          <input type='text' placeholder='Book Title' name='title' value={title} onChange={e => setTitle(e.target.value)} />
+          <input type='number' placeholder='Release date' value={release_year} onChange={e => setReleaseYear(e.target.value)} />
+          <button type='submit'> Add Book </button>
+        </form>
 
       </div>
+      {books.map((book) =>
+        <div>
+          <p>Title: {book.title}</p>
+          <p>Release Year: {book.release_year}</p>
+
+        </div>)}
     </>
   )
 }
