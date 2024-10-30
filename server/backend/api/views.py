ea113c64d4790a006e7f6fa9bde2d7e25afc6733
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-10-30 18:36:17 +0200

    delete and put methods

--- server/backend/api/views.py
+++ server/backend/api/views.py
@@ -22,3 +22,23 @@ def create_book(request):
         serializer.save()
         return Response(serializer.data,status=status.HTTP_201_CREATED)
     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT', 'DELETE']) 
+def book_details(request,id):
+    try:
+        book = Book.objects.get(pk=id)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method =='DELETE':
+        book.delete()
+        print("here")
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method =='PUT':
+        data= request.data
+        serializer =BookSerializer(book, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
