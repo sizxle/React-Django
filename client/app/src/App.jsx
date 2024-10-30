e4b90906ddc629e18662c173d9252f6a628c1af4
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-10-30 16:54:28 +0200

    main page

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,6 +1,28 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const[books,setBooks] = useState([]);
+
+  useEffect(()=>{
+    console.log("before use effect")
+    fetchBooks();
+    console.log("after use effect ")
+  },[])
+
+  const fetchBooks = async()=>{
+
+    try{
+      const response =await fetch("http://127.0.0.1:8000/api/books/");
+      const data = await response.json()
+      console.log(data)
+
+    }catch(err){
+      console.log(err)
+    }
+
+  }
 
   return (
     <>
